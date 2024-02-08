d30521475d75b080456a2fc43b7e7a66ff8cbe6c
Natalie Gillam <ngillam@hotmail.co.uk>
2024-02-08 22:51:09 +0000

    added attribution/copyright information

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,6 +1,15 @@
+
 const Quiz = () => {
   return (
-    "Hello from Quiz"
+    <p class="attribution">
+      The questions on this website contain public sector information licensed
+      under <pre>the </pre> 
+      <a
+        class="attribution-link"
+        tabindex="-1"
+        href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">Government Licence v3.0</a>
+      
+    </p>
   )
 }
 
