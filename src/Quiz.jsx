93c5c90c2de072262494c499cf52d3081e89b2ea
Natalie Gillam <ngillam@hotmail.co.uk>
2024-02-10 00:48:27 +0000

    displayed answer options

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,27 +1,52 @@
-import {useState} from "react";
+import { useState } from "react";
 
-const Quiz = ({questions}) => {
-    const [currentQuestion, setCurrentQuestion] = useState(0);
-    const {question, choices, correctAnswer} = questions[currentQuestion];
+const Quiz = ({ questions }) => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const { question, choices, correctAnswer } = questions[currentQuestion];
+  const [answerIndex, setAnswerIndex] = useState(null);
+  const [answer, setAnswer] = useState(null);
+
+  const onAnswerClick = (answer, index) => {
+    setAnswerIndex(index);
+    if (answer === correctAnswer) {
+      setAnswer(true);
+    } else {
+      setAnswer(false);
+    }
+  };
 
   return (
-    <div className="quiz-container">
+    <>
+      <div className="quiz-container">
         <>
-        <span className="active-question-no">{currentQuestion + 1}</span>
-        <span className="total-questions">/{questions.length}
+          <span className="active-question-no">{currentQuestion + 1}</span>
+          <span className="total-questions">/{questions.length}</span>
+          <h2>{question}</h2>
+          <ul>
+            {choices.map((answer, index) => (
+              <li
+                onclick={() => onAnswerClick(answer, index)}
+                key={answer}
+                className={answerIndex === index?"selected-answer": null}
 
-        </span>
+              >{answer}</li>
+            ))}
+          </ul>
         </>
-    <p class="attribution">
-      The questions on this website contain public sector information licensed
-      under <pre>the </pre> 
-      <a
-        class="attribution-link"
-        tabindex="-1"
-        href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">Government Licence v3.0</a>      
-    </p>
-    </div>
-  )
-}
+      </div>
+      <p class="attribution">
+        The questions on this website contain public sector information licensed
+        under <pre>the </pre>
+        <a
+          class="attribution-link"
+          tabindex="-1"
+          href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/"
+        >
+          Government Licence v3.0
+        </a>
+      </p>
+    </>
+  );
+};
 
-export default Quiz
+export default Quiz;
--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,15 @@
 export const mathsQuiz = {
   questions: [
     {
+        id: 0,
+        question:
+          "Which clock shows 5 minutes past 11?",
+        choices: [4, 5, 10, 20],
+        type: "MCQ",
+        correctAnswer: 20,
+      },
+    {
+        id: 1,
       question:
         "There are 25 sheets of paper in a small pack. There are 500 sheets in a large pack. How many small packs make one large pack?",
       choices: [4, 5, 10, 20],
