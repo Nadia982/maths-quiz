138387479de5e185328fdb6bf52525739cd69ce6
Natalie Gillam <ngillam@hotmail.co.uk>
2024-02-22 23:50:07 +0000

    added showResult variable. Styling updates

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -2,11 +2,14 @@ import { useState } from "react";
 import { resultInitialState } from "./constants";
 
 
+
 const Quiz = ({ questions }) => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [answerIndex, setAnswerIndex] = useState(null);
   const [answer, setAnswer] = useState(null);
   const [result, setResult] = useState(resultInitialState);
+  const [showResult, setShowResult] = useState(false);
+
   const { question, choices, correctAnswer } = questions[currentQuestion];
   
   const onAnswerClick = (answer, index) => {
@@ -27,17 +30,19 @@ const onClickNext = () => {
       ...prev, wrongAnswers: prev.wrongAnswers + 1
     }
   )
-  if(currentQuestion === questions.length - 1) {
+
+  if(currentQuestion !== questions.length - 1) {
     setCurrentQuestion((prev)=> prev + 1)
   } else {
     setCurrentQuestion(0);
+    setShowResult(true);
   }
 }
 
   return (
     <>
       <div className="quiz-container">
-        <>
+        {!showResult ? (<>
           <span className="active-question-no">{currentQuestion + 1}</span>
           <span className="total-questions">/{questions.length}</span>
           <h2>{question}</h2>
@@ -57,20 +62,30 @@ const onClickNext = () => {
               {currentQuestion ===questions.length -1 ? "Finish" : "Next"}
             </button>
           </div>
-        </>
-      </div>
-      <p class="attribution">
-        The questions on this website contain public sector information licensed
-        under <pre>the </pre>
-        <a
-          class="attribution-link"
-          tabindex="-1"
-          href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/"
-        >
-          Government Licence v3.0
-        </a>
-      </p>
-    </>
+        </>) : <div className="result">
+          <h3>Result</h3>
+          <p>Total questions: <span>{questions.length}</span></p>
+          <p>Total correct answers: <span>{result.score}</span></p>
+          <p>Total incorrect answers: <span>{result.wrongAnswers}</span></p>
+              <button className="try-again">Try again</button>
+        </div>}
+        
+      
+
+      
+            <p class="attribution">
+            The questions on this website contain public sector information licensed
+            under <pre>the </pre>
+            <a
+              class="attribution-link"
+              tabindex="-1"
+              href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/"
+            >
+              Government Licence v3.0
+            </a>
+          </p>
+          </div>
+          </>
   );
 };
 
